ba4c35df1b85f4048984c4a461a18d3be0bf1ec4
sj0618 <ktx0618@naver.com>
2024-05-05 00:42:03 +0900

    5/5

--- 열혈 자료구조/자료구조 소스파일/Ch07/ListBaseQueue/ListBaseQueue.c
+++ 열혈 자료구조/자료구조 소스파일/Ch07/ListBaseQueue/ListBaseQueue.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include "ListBaseQueue.h"
 
+
 void QueueInit(Queue * pq)
 {
 	pq->front = NULL;
 	pq->rear = NULL;
 }
-
 int QIsEmpty(Queue * pq)
 {
 	if(pq->front == NULL)
@@ -16,43 +16,35 @@ int QIsEmpty(Queue * pq)
 		return FALSE;
 }
 
-void Enqueue(Queue * pq, Data data)
-{
+void Enqueue(Queue * pq, Data data){
 	Node * newNode = (Node*)malloc(sizeof(Node));
-	newNode->next = NULL;
 	newNode->data = data;
-
-	if(QIsEmpty(pq))
-	{
+	newNode->next = NULL;
+	//만약 첫번째 노드라면.
+	if(QIsEmpty(pq)){
 		pq->front = newNode;
 		pq->rear = newNode;
 	}
-	else
-	{
+	else{
 		pq->rear->next = newNode;
 		pq->rear = newNode;
 	}
 }
-
-Data Dequeue(Queue * pq)
-{
-	Node * delNode;
-	Data retData;
-
-	if(QIsEmpty(pq))
-	{
-		printf("Queue Memory Error!");
+Data Dequeue(Queue * pq){
+	Node * tmpnode;
+	Data tmpdata;
+	if(QIsEmpty(pq)){					//비었을경우 오류 
+		printf("오류");
 		exit(-1);
 	}
-
-	delNode = pq->front;
-	retData = delNode->data;
-	pq->front = pq->front->next;
-
-	free(delNode);
-	return retData;
+	//front를 삭제하고 한칸옮김.
+	tmpdata = pq->front->data;
+	tmpnode = pq->front;
+	pq->front = pq->front->next; 		//front 옮김.data도 옮김 완료
+	
+	free(tmpnode);
+	return tmpdata;
 }
-
 Data QPeek(Queue * pq)
 {
 	if(QIsEmpty(pq))
